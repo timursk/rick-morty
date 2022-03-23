bf53398a0a5c5546a49d10e5cddacd0f7a9c96b8
timursk <timur.dev@mail.ru>
2022-03-23 23:28:13 +0300

    feat: add input lifecycle methods

--- my-app/src/routes/Main.tsx
+++ my-app/src/routes/Main.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import './styles.css';
 
-const Main = () => {
-  return (
-    <div className="input-container">
-      <input type="text" name="input" placeholder="Insert text" className="input" />
-    </div>
-  );
-};
+type MainProps = Record<string, never>;
+type MainState = { value: string; defaultValue: string };
+
+class Main extends React.Component<MainProps, MainState> {
+  constructor(props: MainProps) {
+    super(props);
+    this.state = { defaultValue: '', value: '' };
+  }
+
+  componentDidMount() {
+    const str = localStorage.getItem('input');
+    if (str) {
+      this.setState({
+        defaultValue: str,
+        value: str,
+      });
+    }
+    window.addEventListener('beforeunload', this.componentWillUnmount.bind(this));
+  }
+
+  componentWillUnmount() {
+    localStorage.setItem('input', this.state.value);
+    window.removeEventListener('beforeunload', this.componentWillUnmount.bind(this));
+  }
+
+  set(item: string) {
+    this.setState({
+      value: item,
+    });
+  }
+
+  render() {
+    return (
+      <div className="input-container">
+        <input
+          type="text"
+          name="input"
+          className="input"
+          autoComplete="off"
+          defaultValue={this.state.defaultValue}
+          onChange={(ev) => {
+            this.set(ev.target.value);
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 export default Main;
