97226221adc7e4977a0e5c1eda8f93d6978fdc5f
timursk <timur.dev@mail.ru>
2022-03-25 19:03:49 +0300

    feat: add localstorage mock test

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { getByRole, render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { LocalStorageMock } from '@react-mock/localstorage';
+import userEvent from '@testing-library/user-event';
 
 describe('App', () => {
+  
+
   test('renders after loading', async () => {
     render(
       <BrowserRouter>
@@ -38,5 +42,20 @@ describe('App', () => {
     await screen.findByRole('textbox');
     expect(window.localStorage.getItem).toHaveBeenCalledTimes(1);
     // expect(screen.getByRole('textbox')).toHaveValue('3232');
+  });
+
+  test('mock localstorage', async () => {
+    render(
+      <LocalStorageMock items={{ input: '' }}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </LocalStorageMock>
+    );
+    await screen.findByRole('textbox');
+    expect(screen.getByRole('textbox')).toHaveValue('');
+    userEvent.type(screen.getByRole('textbox'), 'React1234');
+    userEvent.click(screen.getByText('About'));
+    expect(localStorage.getItem('input')).toBe('React1234');
   });
 });
