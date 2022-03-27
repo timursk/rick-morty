656d06ddda1121761ea313cb619c905c2b1d0165
unknown <timur.dev@mail.ru>
2022-03-28 01:35:05 +0300

    refactor: resolve lint warnings

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom';
-import Error404 from './routes/Error404';
-import Main from './routes/Main';
-import About from './routes/About';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
