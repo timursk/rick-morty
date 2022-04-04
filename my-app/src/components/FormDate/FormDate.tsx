0c792e41f538c38994d3b70853c7a6bed3ddb287
timursk <timur.dev@mail.ru>
2022-04-05 01:18:41 +0300

    fix: lint warnings

--- my-app/src/components/FormDate/FormDate.tsx
+++ my-app/src/components/FormDate/FormDate.tsx
@@ -1,4 +1,4 @@
-import React, { Component, RefObject } from 'react';
+import React, { Component } from 'react';
 import { FormItemProps } from '../../utils/types';
 import FormLabel from '../FormLabel/FormLabel';
 
