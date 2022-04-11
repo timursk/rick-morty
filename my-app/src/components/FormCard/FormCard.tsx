bb2b9a9baa37da4aff8f6218bb2735cb14359074
unknown <timur.dev@mail.ru>
2022-04-12 02:37:28 +0300

    refactor: rewrite formCard & formLabel to hooks

--- my-app/src/components/FormCard/FormCard.tsx
+++ my-app/src/components/FormCard/FormCard.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './FormCard.css';
 
 type Props = {
@@ -8,26 +8,22 @@ type Props = {
   country: string;
   profilePicture: File;
 };
-type State = Record<string, never>;
 
-export default class FormCard extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-  }
+const FormCard = (props: Props) => {
+  const { firstName, lastName, birthDate, country, profilePicture } = props;
+  const url = URL.createObjectURL(profilePicture);
 
-  render() {
-    const { firstName, lastName, birthDate, country, profilePicture } = this.props;
-    const url = URL.createObjectURL(profilePicture);
-    return (
-      <div className="form-card">
-        <img className="form-card__img" src={url} alt="avatar" />
-        <p>
-          {firstName} {lastName}
-        </p>
-        <p>
-          {birthDate}, {country}
-        </p>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="form-card">
+      <img className="form-card__img" src={url} alt="avatar" />
+      <p>
+        {firstName} {lastName}
+      </p>
+      <p>
+        {birthDate}, {country}
+      </p>
+    </div>
+  );
+};
+
+export default FormCard;
--- my-app/src/components/FormLabel/FormLabel.tsx
+++ my-app/src/components/FormLabel/FormLabel.tsx
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React, { ReactNode } from 'react';
 import './FormLabel.css';
 
 type Props = {
   labelClass?: string;
+  children?: ReactNode;
 };
-type State = null;
 
-export default class FormLabel extends Component<Props, State> {
-  render() {
-    return (
-      <label className={`form-item ${this.props.labelClass || ''}`}>{this.props.children}</label>
-    );
-  }
-}
+const FormLabel = (props: Props) => {
+  return <label className={`form-item ${props.labelClass || ''}`}>{props.children}</label>;
+};
+
+export default FormLabel;
